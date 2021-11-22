fc63793334a53f51c965390a56c2e5e7ce01c1d8
dhouha <dhouha.mansour11@gmail.com>
2021-11-22 13:40:07 +0100

    forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,16 @@ import { AuthService } from './auth.service';
 
 @Component({
   templateUrl: './login.component.html',
+  styles: [`
+  em {
+    float:right; color:#E05C65; padding-left:10px
+  }
+  `]
 })
 export class LoginComponent {
   userName;
   password;
+  mouseoverLogin;
   constructor(private authService: AuthService, private router: Router) {}
   login(formValues) {
     this.authService.loginUser(formValues.userName, formValues.password);
