648049999b5ea77da99af970607b95ec24d0fd72
dhouha <dhouha.mansour11@gmail.com>
2021-11-27 12:11:34 +0100

    sorting

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -5,24 +5,40 @@ import { ISession } from '../shared/event.model';
   templateUrl: './session-list.component.html',
   selector: 'session-list',
 })
-export class SessionlistComponent implements OnChanges{
+export class SessionlistComponent implements OnChanges {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
-  visibleSessions: ISession[] = []
+  @Input() sortBy: string;
+  visibleSessions: ISession[] = [];
 
-  ngOnChanges(){
-    if(this.sessions){
-      this.filterSessions(this.filterBy)
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+      this.sortBy === 'name'
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVotesDesc);
     }
   }
 
-  filterSessions(filter){
-    if (filter ==='all'){
-      this.visibleSessions = this.sessions.slice( 0)
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
     } else {
-      this.visibleSessions = this.sessions.filter(session => {
-        return session.level.toLocaleLowerCase() === filter
-      })
+      this.visibleSessions = this.sessions.filter((session) => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
     }
   }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) {
+    return 1;
+  } else if (s1.name === s2.name) {
+    return 0;
+  } else return -1;
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
