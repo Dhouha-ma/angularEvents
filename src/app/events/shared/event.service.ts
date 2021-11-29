720a74aa2959a25cb182c1bff02c453644442159
dhouha <dhouha.mansour11@gmail.com>
2021-11-29 16:58:17 +0100

    moving data to server

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,16 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { IEvent, ISession } from './event.model';
 
 @Injectable()
 export class EventService {
+  constructor(private http: HttpClient) {}
+
   getEvents(): Observable<IEvent[]> {
-    let subject = new Subject<IEvent[]>();
-    setTimeout(() => {
-      subject.next(EVENTS);
-      subject.complete();
-    }, 100);
-    return subject;
+    return this.http
+      .get<IEvent[]>('/api/events')
+      .pipe(catchError(this.handleError<IEvent[]>('getEvents', [])));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
   }
 
   getEvent(id: number): IEvent {
@@ -33,8 +41,9 @@ export class EventService {
     var results: ISession[] = [];
 
     EVENTS.forEach((event) => {
-      var matchingSessions = event.sessions.filter(session => 
-        session.name.toLocaleLowerCase().indexOf(term) > -1);
+      var matchingSessions = event.sessions.filter(
+        (session) => session.name.toLocaleLowerCase().indexOf(term) > -1
+      );
       matchingSessions = matchingSessions.map((session: any) => {
         session.eventId = event.id;
         return session;
@@ -46,8 +55,8 @@ export class EventService {
     setTimeout(() => {
       emitter.emit(results);
     }, 100);
-     
-    return emitter
+
+    return emitter;
   }
 }
 
